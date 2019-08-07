cb423d5d24d8490101eb68e8166e9a1a97ddec50
jhonarielgj <jhonarielgj@yahoo.com>
2019-08-07 13:20:24 -0500

    Update villa.js

--- platzi/js/villa.js
+++ platzi/js/villa.js
@@ -27,6 +27,10 @@ let vaca = {
 
 
 //INSERTAR UNA IMAGEN EN UN CAVNAS
+let numVacas  = aleatorio(0, 40);
+let numCerdos = aleatorio(0, 2);
+let numPollos = aleatorio(0, 6);
+
 fondo.imagen = new Image();  //buena práctica los nombres de clases empieza con mayúscula
 fondo.imagen.src = fondo.url;
 fondo.imagen.addEventListener("load", cargarFondo);
@@ -49,19 +53,46 @@ function cargarFondo() {
 }
 function cargarCerdo() {
     cerdo.cargaOk = true;
+    dibujar();
 }
 
 function cargarPollo() {
     pollo.cargaOk = true;
+    dibujar();
 }
 
 function cargarVaca() {
     vaca.cargaOk = true;
+    dibujar();
 }
 
 function dibujar() {
-    if (fondo.cargaOk == true) {
-        papel.drawImage(fondo, 0, 0);
+    if (fondo.cargaOk) {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+    if (vaca.cargaOk) {
+        for (let v = 0; v < numVacas; v++) {
+            let x = aleatorio(0, 7);
+            x = x * 60; //genera columnas para ubicar las vacas
+            let y = aleatorio(0, 7);
+            y = y * 60; //genera filas para ubicas las vacas y formar una grilla
+            papel.drawImage(vaca.imagen, x, y);
+        }
+    }
+    if (cerdo.cargaOk) {
+        for (let v = 0; v < numCerdos; v++) {
+            let x = aleatorio(0, 420);
+            let y = aleatorio(0, 420);
+            papel.drawImage(vaca.imagen, x, y);
+        }
+        papel.drawImage(cerdo.imagen, 100, 100);
+    }
+    if (pollo.cargaOk) {
+        for (let v = 0; v < numPollos; v++) {
+            let x = aleatorio(0, 420);
+            let y = aleatorio(0, 420);
+            papel.drawImage(pollo.imagen, x, y);
+        }
     }
 }
 
