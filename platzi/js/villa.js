3b70463170d9d51922a2f34e1f040e0cb31e208f
jhonarielgj <jhonarielgj@yahoo.com>
2019-08-07 12:44:06 -0500

    7/8/19

--- platzi/js/villa.js
+++ platzi/js/villa.js
@@ -6,8 +6,68 @@ Math.floor(número) => redondea hacia el entero más bajo
 Math.random() => número aleatorio entre 0 y 1
 */
 
-aleatorio (min, max) => {
-    let resultado = Math.ceil(Math.random() + (max - min + 1)) + min;
+let vp = document.getElementById("villaPlatzi");
+let papel = vp.getContext("2d");
+let fondo = {
+    url: "img/tile.png",
+    cargaOk: false
+}    
+let cerdo = {
+    url: "img/cerdo.png",
+    cargaOk: false
+}    
+let pollo = {
+    url: "img/pollo.png",
+    cargaOk: false
+}
+let vaca = {
+    url: "img/vaca.png",
+    cargaOk: false
+}
+
+
+//INSERTAR UNA IMAGEN EN UN CAVNAS
+fondo.imagen = new Image();  //buena práctica los nombres de clases empieza con mayúscula
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+cerdo.imagen = new Image();  
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
+
+pollo.imagen = new Image();  
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollo);
+
+vaca.imagen = new Image();  
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVaca);
+
+function cargarFondo() {
+    fondo.cargaOk = true;
+    dibujar();
+}
+function cargarCerdo() {
+    cerdo.cargaOk = true;
+}
+
+function cargarPollo() {
+    pollo.cargaOk = true;
+}
+
+function cargarVaca() {
+    vaca.cargaOk = true;
+}
+
+function dibujar() {
+    if (fondo.cargaOk == true) {
+        papel.drawImage(fondo, 0, 0);
+    }
+}
+
+function aleatorio(min, max) {
+    let resultado;
+    resultado = Math.floor(Math.random() * (max - min + 1)) + min;
     return resultado;
 }
 
