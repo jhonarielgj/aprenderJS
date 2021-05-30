f1f14476fd7940ff1b1e3a92b688a7e219df7ae3
Jhon Gomez <jhonarielgj@yahoo.com>
2021-05-30 15:18:53 -0500

    ejercicios jon mircha

--- jonMircha/js/index_dom.js
+++ jonMircha/js/index_dom.js
@@ -1,7 +1,9 @@
 import hamburguerMenu from "./menu_hamburguesa.js";
+import {digitalClock, alarm} from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", e => {
     hamburguerMenu(".panel-btn",".panel", ".menu a");
+    digitalClock("#reloj","#activar-reloj", "#desactivar-reloj");
 })
